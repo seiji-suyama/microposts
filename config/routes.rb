c911b4db60d5247a78f02bd483e973bd9c32f766
seiji_suyama <s.e.i.j.i.8-7@hotmail.co.jp>
2016-02-07 09:56:05 +0000

    user-profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get 'signup', to: 'users#new'
-  
-  resources :users
-end
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+end
 
  
 
